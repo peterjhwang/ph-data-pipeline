5539bc8061fc36066107abcf9b5c20490508be90
peterjhwang <after10y@gmail.com>
2022-07-18 08:32:32 +1200

    elastic beanstalk setup

--- controllers/stats_controllers.py
+++ controllers/stats_controllers.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from flask_app import application
+from utils.aws.sns import send_message
 from services.stats.stats_api_service import stats_api_refresh 
 
 @application.route('/stats_api')
@@ -7,6 +8,8 @@ def refresh_stats_api():
     application.logger.info('/stats_api')
     try:
         stats_api_refresh()
-        return jsonify({'message': 'Stats API data successfully updated'})
+        send_message("INFO", 'Cache file successfully refreshed.\n\nph-data-pipeline')
+        return jsonify({'message': 'Stats data successfully refreshed'})
     except Exception as e:
+        send_message("ERROR", str(e) + '\n\nh-data-pipeline')
         return jsonify({'Error message': str(e)})
